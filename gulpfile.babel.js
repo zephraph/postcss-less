ceddc043843475a2d06ab439602aa7e7b3ab7e90
Brandon Foster <bfoster@scrippsnetworks.com>
2015-11-14 22:00:26 -0600

    Replacing es6 references with es

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ gulp.task('clean', () => {
 
 gulp.task('build:lib', ['clean'], () => {
     let babel = require('gulp-babel');
-    return gulp.src('lib/*.es6')
+    return gulp.src('lib/*.es')
         .pipe(babel())
         .pipe(gulp.dest('build/lib'));
 });
@@ -41,7 +41,7 @@ gulp.task('build', ['build:lib', 'build:docs', 'build:package']);
 
 gulp.task('lint', () => {
     let eslint = require('gulp-eslint');
-    return gulp.src(['*.js', 'lib/*.es6', 'test/*.es6'])
+    return gulp.src(['*.js', 'lib/*.es', 'test/*.es'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -50,13 +50,13 @@ gulp.task('lint', () => {
 // Test
 
 gulp.task('test', () => {
-    require('babel-core/register')({ extensions: ['.es6'], ignore: false });
+    require('babel-core/register')({ extensions: ['.es'], ignore: false });
     let mocha = require('gulp-mocha');
-    return gulp.src('test/*.es6', { read: false }).pipe(mocha());
+    return gulp.src('test/*.es', { read: false }).pipe(mocha());
 });
 
 gulp.task('integration', (done) => {
-    require('babel-core/register')({ extensions: ['.es6'], ignore: false });
+    require('babel-core/register')({ extensions: ['.es'], ignore: false });
     let postcss = require('postcss');
     let real    = require('postcss-parser-tests/real');
     let scss    = require('./');
